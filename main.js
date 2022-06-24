748aa13190d327d4c0cfcc6c90722b9b1db59c24
pradeep <pradeep@cdtech.in>
2022-06-24 15:55:03 +0530

    Initial commit

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function add() {
 // PUT/PATCH REQUEST
 function update() {
   axios
-    .patch('https://jsonplaceholder.typicode.com/todos/1',)
+    .put('https://jsonplaceholder.typicode.com/todos/1',)
     .then(res => showOutput(res))
     .catch(err => console.error(err));
 }
@@ -40,46 +40,7 @@ function remove() {
     .then(res => showOutput(res))
     .catch(err => console.error(err));
 }
-
-
-// Show output in browser
-function showOutput(res) {
-    document.getElementById('res').innerHTML = `
-    <div class="card card-body mb-4">
-      <h5>Status: ${res.status}</h5>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Headers
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
-      </div>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Data
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.data, null, 2)}</pre>
-      </div>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Config
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.config, null, 2)}</pre>
-      </div>
-    </div>
-  `;
-  }
-
  
-  
 
 // Event listeners
 document.getElementById('get').addEventListener('click', get);
